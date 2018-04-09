09f13fe5e95bd56c18947685dabb7836a5ad251a
lyin <ling95613@gmail.com>
2018-04-09 22:08:29 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>todos</h1>
+    <div class="input-box">
+      <input placeholder="请输入内容" @keyup.enter="addTodo"/>
+    </div>
+    <Item
+      v-for="todo in todos"
+      :todo="todo"
+      :key="todo.id"
+      :onDelete="onDelete"
+     />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Item from "./components/Item.vue";
 
+let id = 2;
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Item
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          content: "one ",
+          completed: false
+        }
+      ]
+    };
+  },
+  methods: {
+    // 添加一条todo 事项
+    addTodo(e) {
+      this.todos.unshift({
+        id: id++,
+        content: e.target.value,
+        completed: false
+      });
+      e.target.value = "";
+    },
+    // 删除一条todo事项
+    onDelete(id) {
+      this.todos = this.todos.filter(val => val.id !== id);
+      console.log("you want delete", id);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+.input-box {
+  margin-bottom: 20px;
+}
+.input-box input {
+    width: 260px;
+    height: 30px;
+    line-height: 30px;
+    font-size: 15px;
+    /* border-radius: 5px; */
+    border: 0;
+    outline: 0;
+    border-bottom: 1px solid #333;
+  }
 </style>
---
+++ src/components/Item.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="item">
+    <span>{{todo.content}}</span>
+    <span class="btn delete" @click="onDelete(todo.id)">X</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Item',
+  props:{
+    todo:{
+      type:Object,
+      required:true
+    },
+    onDelete:{
+      type:Function
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  .item:first-child{
+    margin-top: 20px;
+  }
+  .item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: auto;
+    width: 260px;
+    padding: 6px 5px;
+    border-bottom: 1px solid #ccc;
+  }
+  .btn{
+    cursor: pointer;
+  }
+  .delete{
+    color: red;
+  }
+</style>
+
